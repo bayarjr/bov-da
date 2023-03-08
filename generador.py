ff5143b996683e835c1ea93bf4a06c404c163d58
micagiss <mgissellatrujillo@gmail.com>
2023-03-07 21:05:59 -0500

    mica

---
+++ generador.py
@@ -0,0 +1,10 @@
+def main():
+
+    ventana = tk.Tk()
+    # Crea la instancia de la clase VentanaPrincipal
+    VentanaPrincipal(ventana)
+    # Ejecuta la ventana
+    ventana.mainloop()
+
+if __name__ == "__main__":
+    main()
